a7d90c908e4f479954d110d914f2953e7845821d
vielgi <vgeorgiev@alumni.harvard.edu>
2018-02-26 16:50:28 -0600

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -31,9 +31,9 @@ class Base {
 
     protected void run() {
         double r = Math.random();
-        if (r < 0.1) {
+        if (r < 0.05) {
             fail("oops");
-        } else if (r < 0.9) {
+        } else if (r < 0.2) {
             throw new AssumptionViolatedException("skipping");
         }
     }
