839d7a3133044c912a9c62d172f7dc85a1af5a02
vielgi <vgeorgiev@alumni.harvard.edu>
2018-02-26 16:46:40 -0600

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -30,7 +30,7 @@ import org.junit.internal.AssumptionViolatedException;
 class Base {
 
     protected void run() {
-        double r = Math.Random();
+        double r = Math.random();
         if (r < 0.9) {
             fail("oops");
         } else if (r > 0.9) {
